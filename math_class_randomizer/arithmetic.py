ee642e71bcb386b49f24e932a9a5a2260ad6a686
groldo <groldo@dumpme.de>
2023-01-07 08:34:07 +0000

    add docstring

--- math_class_randomizer/arithmetic.py
+++ math_class_randomizer/arithmetic.py
@@ -10,4 +10,8 @@ class ArithmeticEx(Excersise):
             2) for _ in range(count)]
 
     def get_random_ex_values(self, count) -> tuple:
+        """
+        Returns a list of randomly generated values from the given range.
+        The number of values returned can be controlled by the `count` parameter.
+        """
         return [random.randrange(self.config["arithmetic"]["min"], self.config["arithmetic"]["max"]) for _ in range(count)]
